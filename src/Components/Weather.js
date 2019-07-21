db5bc55be9bb0718b6afccb9d5645f63574144f3
Siddhant Bohra <abhisidds@gmail.com>
2019-07-21 19:08:57 +0530

    Updated Component

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,9 +5,9 @@ class Weather extends Component {
         return (
             <div>
                 {this.props.city && this.props.country && <p>Location: {this.props.city} , {this.props.country}</p>}
-                {this.props.temprature && <p>Temprature : this.props.temprature</p>}
-                {this.props.humidity && <p>Humidity : this.props.humidity </p>}
-                {this.props.description && <p>Description : this.props.description</p>}
+                {this.props.temprature && <p>Temprature : {this.props.temprature}</p>}
+                {this.props.humidity && <p>Humidity : {this.props.humidity} </p>}
+                {this.props.description && <p>Description : {this.props.description}</p>}
             </div>
         )
     }
