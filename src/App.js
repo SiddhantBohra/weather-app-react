fbe97f8c6a0dcf0c7e975e3e3fc6d31304d69bff
Siddhant Bohra <abhisidds@gmail.com>
2019-07-21 23:16:26 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
     if (city && country) {
       const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
       const data = await api_call.json()
-      console.log(data)
       this.setState({
         temprature: data.main.temp,
         city: data.name,
@@ -34,7 +33,6 @@ class App extends Component {
     else if (city) {
       const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
       const data = await api_call.json()
-      console.log(data)
       this.setState({
         temprature: data.main.temp,
         city: data.name,
